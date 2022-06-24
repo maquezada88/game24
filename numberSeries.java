baa703b72cae52b3e62356a417084386a1905f70
maquezada88 <107223686+maquezada88@users.noreply.github.com>
2022-06-23 20:37:14 -0400

    added usage and convert methods

--- numberSeries.java
+++ numberSeries.java
@@ -77,7 +77,7 @@ class numberSeries {
 		return null;
 	}
 	
-	int calc1(int op1, int op2, char op)
+	static int calc(int op1, int op2, char op)
 	{
 		int result = 0;
 		
@@ -105,25 +105,7 @@ class numberSeries {
 		return result;
 	}
 	
-	static Triplet removeNum(int val, Quartet nums)
-	{
-		Triplet<Integer, Integer, Integer> trips;
-		int counter;
-		System.out.println("REMOVE MOETHOD");
-		
-			if( (Integer)val == nums.getValue0())
-				trips = nums.removeFrom0();
-			else if( (Integer)val == nums.getValue1())
-				trips = nums.removeFrom1();
-			else if( (Integer)val == nums.getValue2())
-				trips = nums.removeFrom2();
-			else
-				trips = nums.removeFrom3();
-			
-			return trips;
-				
-	}
-	
+	//check expression is numbers and valid operators
 	static boolean isExpressionValid(String expression, Character[] operators)
 	{
 		
@@ -137,6 +119,65 @@ class numberSeries {
 		return false;
 	}
 	
+	//check number inputs are in valid choices.
+	static boolean isInputValid(Quartet nums, int num1, int num2)
+	{
+		Triplet<Integer, Integer, Integer> temp;
+		
+		if(! nums.contains((Integer)num1)) 
+		{
+			System.out.println("Number1 "+num1+" is not valid.");
+			return false;
+		}
+		else 
+		{
+			if( (Integer)num1 == nums.getValue0())
+				temp = nums.removeFrom0();
+			else if( (Integer)num1 == nums.getValue1())
+				temp = nums.removeFrom1();
+			else if( (Integer)num1 == nums.getValue2())
+				temp = nums.removeFrom2();
+			else
+				temp = nums.removeFrom3();
+		}
+		
+		if(! temp.contains((Integer)num2)) 
+		{
+			System.out.println("Number2 "+num2+" is not valid.");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	static Triplet convertToTriplet(Triplet trip, Quartet nums, Integer num1, Integer num2, char op)
+	{
+		Triplet<Integer, Integer, Integer> temp;
+		int result = calc(num1, num2, op);
+		
+		//remove num1 and create temporary triplet
+		if( (Integer)num1 == nums.getValue0())
+			temp = nums.removeFrom0();
+		else if( (Integer)num1 == nums.getValue1())
+			temp = nums.removeFrom1();
+		else if( (Integer)num1 == nums.getValue2())
+			temp = nums.removeFrom2();
+		else
+			temp = nums.removeFrom3();
+		
+		//remove num2 from temporary triplet and add result
+		if( (Integer)num2 == nums.getValue0())
+			trip = temp.removeFrom0().addAt0((Integer)result);
+		else if( (Integer)num2 == nums.getValue1())
+			trip = temp.removeFrom1().addAt1((Integer)result);
+		else if( (Integer)num2 == nums.getValue2())
+			trip = temp.removeFrom2().addAt2((Integer)result);
+		
+		return trip;
+	}
+	
+	//converts the arithmetic string input from user
+	//into num1 num2 and op
 	static void convertInput(String expression)
 	{
 		String input = expression.replaceAll(" ", "");
@@ -144,6 +185,15 @@ class numberSeries {
 		game.num2 = Character.getNumericValue(input.charAt(2));
 		game.op = input.charAt(1);
 		
+	}
+	
+	static void usage(Object obj)
+	{
+		
+		System.out.println("Use addition, subtraction, division, or multiplication to get the sum of 24!");
+		System.out.println("List of numbers available to use:");
+		System.out.println(obj);
+		System.out.println("Enter an expression.");
 	}
 	
 }
